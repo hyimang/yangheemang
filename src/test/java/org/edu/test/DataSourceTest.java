5481aacb9eeb69cf21609606e8c04bae1b52628a
Administrator <Administrator@MSDN-SPECIAL>
2020-12-14 10:49:44 +0900

    20201214 Eclipse Update

---
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -0,0 +1,17 @@
+package org.edu.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DataSourceTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
