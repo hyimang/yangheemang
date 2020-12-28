c7b2ab9007b9d18857b476350126a96d41e06b9a
Administrator <Administrator@MSDN-SPECIAL>
2020-12-28 11:04:28 +0900

    20201228 관리자단html jsp 작업 시작

---
+++ src/main/java/org/edu/dao/BoardDAOImpl.java
@@ -0,0 +1,28 @@
+package org.edu.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.edu.vo.BoardVO;
+import org.edu.vo.PageVO;
+import org.springframework.stereotype.Repository;
+
+@Repository//아래 DAO만클래스를 레포지터리(저장소)애노테이션으로 감싸줌으로써 여러 내용 사용가능함
+public class BoardDAOImpl implements IF_BoardDAO {
+
+	@Inject //sql세션템플릿을 주입 인젝션 받아서 변수로 지정
+	private SqlSession sqlSession;
+	
+	
+	@Override
+	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception {
+		// sql세션 템플릿(select,update,delete 같은 메서드가 포함)매페쿼리 지정(아래)
+		//sql세션 템플릿? root-context.xml안에 지정한 템플릿
+		//매퍼쿼리(위쿼리+insert등)을 실행할때 개발자가 DB커넥션, 디스커넥션 생각할 필요없이 사용가능한
+		//매서드 집합을 구성해 놓은 것이  sql 세션템플릿입니다.
+		return sqlSession.selectList("boardMapper.selectBoard", pageVO);
+	}
+
+}
